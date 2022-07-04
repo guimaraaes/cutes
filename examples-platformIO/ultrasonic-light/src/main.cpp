8ea1043d6f59733a23f1f9ae1c8a00d425c5deeb
guimaraaes <sguimaraaes@gmail.com>
2022-07-04 17:28:01 -0300

    improve: memory analyse to create and test

--- examples-platformIO/ultrasonic-light/src/main.cpp
+++ examples-platformIO/ultrasonic-light/src/main.cpp
@@ -5,28 +5,28 @@
 // #include "../lib/ArduinoUnit/src/ArduinoUnit.h"
 // #include "../test/test_ArduinoUnit.cpp"
 
-#include "../test/test_AUnit.cpp"
-#include "AUnit/src/AUnit.h"
-using namespace aunit;
+// #include "../test/test_AUnit.cpp"
+// #include "AUnit/src/AUnit.h"
+// using namespace aunit;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  // ultrasonic_light.setup();
+  ultrasonic_light.setup();
 
   // casesTest->setup();
 
-  TestRunner::exclude("*");
-  TestRunner::include("TestUltrasonic", "*");
+  // TestRunner::exclude("*");
+  // TestRunner::include("TestUltrasonic", "*");
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  // ultrasonic_light.loop();
+  ultrasonic_light.loop();
   // casesTest->loop();
   // Test::run();
 
-  TestRunner::run();
+  // TestRunner::run();
 }
