9095f10b7eb8aeb5b724580cf9c19fd9d0c94a94
guimaraaes <sguimaraaes@gmail.com>
2022-07-08 22:19:52 -0300

    improve: pir light example with embedded system folder

---
+++ examples-platformIO/pir-light/embedded_system/embedded_system.cpp
@@ -0,0 +1,37 @@
+#include "../../../unit-embedded-system-framework/embedded_system/EmbeddedSystem.h"
+#include "../../../unit-embedded-system-framework/component/units-creatores/CreatorActuatorDigital.h"
+#include "../../../unit-embedded-system-framework/component/units-creatores/CreatorSensorDigital.h"
+
+class Arduino : public EmbeddedSystem
+{
+
+public:
+    ActuatorDigital *light;
+    SensorDigital *pir;
+
+    Arduino(String description, String author) : EmbeddedSystem(description, author){};
+    void configuration(){};
+
+    void setup()
+    {
+        CreatorActuatorDigital *creator_light = new CreatorActuatorDigital();
+        light = creator_light->createComponent(13);
+
+        CreatorSensorDigital *creator_pir = new CreatorSensorDigital();
+        pir = creator_pir->createComponent(3);
+    };
+    void loop()
+    {
+        if (pir->read() == HIGH)
+        {
+            light->write(HIGH);
+            delay(1000);
+        }
+        if (pir->read() == LOW)
+        {
+            light->write(LOW);
+            delay(1000);
+        }
+    };
+};
+Arduino pir_light = Arduino("sistema embarcado acender luz enquanto movimento for detectado", "Sara Guimarães");
--- examples-platformIO/pir-light/src/main.cpp
+++ examples-platformIO/pir-light/src/main.cpp
@@ -1,66 +1,16 @@
 #include <Arduino.h>
-#include "../../../unit-embedded-system-framework/embedded_system/EmbeddedSystem.h"
-class Arduino : public AbstractEmbeddedSystem
-{
-  class CreatorLight : public Creator
-  {
-  public:
-    CreatorLight(){};
-    Component *createComponent(int pin)
-    {
-      return new ActuatorDigital(pin);
-    };
-  };
+#include "../embedded_system/embedded_system.cpp"
 
-  class CreatorPir : public Creator
-  {
-  public:
-    CreatorPir(){};
-    Component *createComponent(int pin)
-    {
-      return new SensorDigital(pin);
-    };
-  };
-
-public:
-  ActuatorDigital *light;
-  SensorDigital *pir;
-
-  void configuration(){};
-
-  void setup()
-  {
-    CreatorLight *creator_light = new CreatorLight();
-    this->light = creator_light->createComponent(13);
-
-    CreatorPir *creator_pir = new CreatorPir();
-    this->pir = creator_pir->createComponent(3);
-  };
-  void loop()
-  {
-    if (this->pir->read() == HIGH)
-    {
-      this->light->write(HIGH);
-      delay(1000);
-    }
-    if (this->pir->read() == LOW)
-    {
-      this->light->write(LOW);
-      delay(1000);
-    }
-  };
-};
-Arduino *pir_light;
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pir_light->setup();
+  pir_light.setup();
 }
 
 void loop()
 {
 
   // put your main code here, to run repeatedly:
-  pir_light->loop();
+  pir_light.loop();
 }
