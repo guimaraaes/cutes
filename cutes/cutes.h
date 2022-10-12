1e75c0cbfd71c4efecd127c04e67d9b8136fa1e6
guimaraaes <sguimaraaes@gmail.com>
2022-10-12 11:47:15 -0300

    feat: add cutes interface includes

---
+++ cutes/cutes.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "embedded_system/EmbeddedSystem.h"
+#include "component/units-creatores/CreatorUltrasonic.h"
+#include "component/units-creatores/CreatorActuatorDigital.h"
+#include "component/units-creatores/CreatorSensorDigital.h"
